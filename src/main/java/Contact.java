d57c70091cce9981d13c63294665b9f21f120de5
Inthrayuth Mahaphol and Nick Lyman <nick@>
2016-04-28 14:48:24 -0700

    Add BDD unit tests to check names being pushed to array, finding unique IDs of contacts, clearing contacts from array between tests, and returning null when contact ID is not found.

--- src/main/java/Contact.java
+++ src/main/java/Contact.java
@@ -3,17 +3,41 @@ import java.util.ArrayList;
 public class Contact {
   private String mFirstName;
   private String mLastName;
-  private String mPhoneNumber;
   private static ArrayList<Contact> instances = new ArrayList<Contact>();
   private int mId;
 
-  public Contact(String firstName, String lastName, String phoneNumber) {
+  public Contact(String firstName, String lastName) {
     mFirstName = firstName;
     mLastName = lastName;
-    mPhoneNumber = phoneNumber;
     instances.add(this);
     mId = instances.size();
   }
 
+  public String getFirstName() {
+    return mFirstName;
+  }
 
+  public String getLastName() {
+    return mLastName;
+  }
+
+  public static ArrayList<Contact> all() {
+    return instances;
+  }
+
+  public static void clear() {
+   instances.clear();
+  }
+
+  public int getId() {
+    return mId;
+  }
+
+  public static Contact find(int id) {
+    try {
+      return instances.get(id - 1);
+    } catch (IndexOutOfBoundsException exception) {
+      return null;
+    }
+  }
 }
--- src/test/java/ContactTest.java
+++ src/test/java/ContactTest.java
@@ -3,9 +3,50 @@ import static org.junit.Assert.*;
 
 public class ContactTest {
 
+  @Rule
+    public ClearRule clearRule = new ClearRule();
+
   @Test
   public void Contact_instantiatesCorrectly_true() {
-    Contact myContact = new Contact("Nick", "Lyman", "555-555-5555");
+    Contact myContact = new Contact("Nick", "Lyman");
     assertEquals(true, myContact instanceof Contact);
+  }
+
+  @Test
+  public void Contact_instantiatesWith_FirstName_String() {
+    Contact myContact = new Contact("Nick", "Lyman");
+    assertEquals("Nick", myContact.getFirstName());
+  }
+
+  @Test
+  public void all_ContactInArray_returnsAllContact_true() {
+    Contact firstContact = new Contact("Nick", "Lyman");
+    Contact secondContact = new Contact("In", "Out");
+    assertTrue(Contact.all().contains(firstContact));
+    assertTrue(Contact.all().contains(secondContact));
+  }
+
+  @Test
+  public void clear_emptiesAllContactsFromArrayList_0() {
+    Contact myContact = new Contact("Nick", "Lyman");
+    Contact.clear();
+    assertEquals(Contact.all().size(), 0);
+  }
+
+  @Test
+  public void getId_contactsInstantiateWithAnID_1() {
+    Contact myContact = new Contact("Nick", "Lyman");
+    assertEquals(1, myContact.getId());
+  }
+
+  @Test
+  public void find_returnsContactWithSameId_secondContact() {
+    Contact firstContact = new Contact("Nick", "Lyman");
+    Contact secondContact = new Contact("In", "Out");
+    assertEquals(Contact.find(secondContact.getId()), secondContact);
+  }
+  @Test
+  public void find_returnsNullWhenNoContactFound_null() {
+    assertTrue(Contact.find(999) == null);
   }
 }
